ccc64c5c69aff23f36b4cadac65d2996fcc3d476
Youngheon Jeong <81753155+jjyyhh0208@users.noreply.github.com>
2025-01-19 17:58:28 +0900

    25/01/19

--- knight_of_king.py
+++ knight_of_king.py
@@ -2,3 +2,19 @@ def alph_to_num(alph: str):
     alph = alph.upper()
     num = ord(alph) - ord('A') + 1
     return num
+
+dx = [-1, -1, 1, 1, 2, -2, 2, -2]
+dy = [2, -2, 2, -2, 1, 1, -1, -1]
+start = input()
+x = alph_to_num(start[0])
+y = int(start[1])
+count = 0
+
+for i in range(8):
+    nx = x + dx[i]
+    ny = y + dy[i]
+    if nx < 1 or nx > 8 or ny < 1 or ny > 8:
+        continue
+    count += 1
+    
+print(count)
